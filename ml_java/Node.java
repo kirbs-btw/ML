ccebbef5e7939ca49aa39356604bdbdb3e9ff4b0
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2022-10-11 17:53:26 +0200

    Add files via upload

---
+++ ml_java/Node.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Node{
+    public List<Float> data = new ArrayList<>();
+
+    public Node(float ... a){
+        for (float dataPoint : a){
+            data.add(dataPoint);
+        }
+
+    }
+
+    public void checkValues(){
+        for (float dataPoint: data){
+            System.out.println(dataPoint);
+
+        }
+    }
+}
