4d7c2826011c61657a80d47e6f1950bdbb92ca80
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2022-10-15 19:34:28 +0200

    Add files via upload

--- kNN_java/Node.java
+++ kNN_java/Node.java
@@ -1,11 +1,10 @@
 import java.util.*;
 
 public class Node{
-    public List<Float> data = new ArrayList<>;
+    public List<Float> data = new ArrayList<>();
     public float value;
 
-
-    public Node(float ... a, float state){
+    public Node(float state, float ... a){
         for (float dataPoint : a){
             data.add(dataPoint);
         }
@@ -15,7 +14,7 @@ public class Node{
     public void checkValues(){
         for (float dataPoint: data){
             System.out.println(dataPoint);
-
+            System.out.println(value);
         }
     }
 }
