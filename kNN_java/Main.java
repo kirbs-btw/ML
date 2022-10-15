085c0263bd0b24e1aa524c853bc0e2ad6c61b5cd
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2022-10-15 19:34:17 +0200

    Add files via upload

--- kNN_java/Main.java
+++ kNN_java/Main.java
@@ -1,19 +1,20 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.*;
+
 
 public class Main {
 
-    public static List<List<Float>> distance(List<Node> inputLayer, List<Node>outputLayer){
+    public static List<List<Float>> distance(List<Node> inputLayer, List<Node> outputLayer){
         List<List<Float>> distSetList = new ArrayList<>();
 
-        for (Node nodeInput : inputLayer){
-
+        for (Node inputNode : inputLayer){
             List<Float> distSet = new ArrayList<>();
-            for (Node nodeOutput : outputLayer){
+            for (Node outputNode : outputLayer){
                 float diffSqareSum = 0f;
-                for (int i = 0; i < nodeInput.data.size(); i++){
-                    float opinionDiff = nodeInput.data.get(i) - nodeOutput.data.get(i);
-                    diffSqareSum = diffSqareSum + opinionDiff * opinionDiff;
+                for (int i = 0; i < inputNode.data.size(); i++){
+                    float diff = inputNode.data.get(i) - outputNode.data.get(i);
+                    diffSqareSum = diffSqareSum + (diff * diff);
                 }
                 float dist = (float) Math.sqrt(diffSqareSum);
                 distSet.add(dist);
@@ -25,13 +26,18 @@ public class Main {
 
 
     public static void main(String[] args) {
+        int k = 50;
         List<Node> outputLayer = new ArrayList<>();
         List<Node> inputLayer = new ArrayList<>();
 
+        for (int i = 0; i < 50; i++){
+            Node a = new Node(0, 1, 2, 3);
+            Node b = new Node(0, 1, 5, 3);
 
-
-
-
+            outputLayer.add(a);
+            inputLayer.add(b);
+        }
+        System.out.println(distance(inputLayer, outputLayer));
 
     }
 }
